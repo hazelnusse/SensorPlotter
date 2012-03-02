b632f91397d40431708883c411d0c42010e90985
Dale Lukas Peterson <hazelnusse@gmail.com>
2012-03-01 18:03:33 -0800

    Lots of stuff added/implemented.

--- src/edu/project/sensorplotter/SensorPlotterActivity.java
+++ src/edu/project/sensorplotter/SensorPlotterActivity.java
@@ -2,16 +2,21 @@ package edu.project.sensorplotter;
 
 import com.androidplot.xy.XYPlot;
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.view.View;
 
-public class SensorPlotterActivity extends Activity {
+
+public class SensorPlotterActivity extends Activity implements SensorEventListener, View.OnClickListener {
 
 	private Spinner sensor, sampleRate;
 	private Button Start, Clear, Stop;
-	private XYPlot sensorDataPlot;
+	//private XYPlot sensorDataPlot;
 	
 	
     @Override
@@ -30,19 +35,49 @@ public class SensorPlotterActivity extends Activity {
     	Start = (Button) findViewById(R.id.Start);
     	Clear = (Button) findViewById(R.id.Clear);
     	Stop = (Button) findViewById(R.id.Stop);
+    	configureButtons();
     	// XY Plot
-    	sensorDataPlot = (XYPlot) findViewById(R.id.sensorDataPlot);
+    	//sensorDataPlot = (XYPlot) findViewById(R.id.sensorDataPlot);
+    	configureDataPlot();
     }
 
+	private void configureDataPlot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void configureButtons() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private void configureSpinners() {
-		ArrayAdapter sensorAdapter = ArrayAdapter.createFromResource(
+		ArrayAdapter<CharSequence> sensorAdapter = ArrayAdapter.createFromResource(
 	            this, R.array.sensor_array, android.R.layout.simple_spinner_item);
 	    sensorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    sensor.setAdapter(sensorAdapter);
 	    
-	    ArrayAdapter sampleRateAdapter = ArrayAdapter.createFromResource(
+	    ArrayAdapter<CharSequence> sampleRateAdapter = ArrayAdapter.createFromResource(
 	            this, R.array.rate_array, android.R.layout.simple_spinner_item);
 	    sampleRateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    sampleRate.setAdapter(sampleRateAdapter);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
